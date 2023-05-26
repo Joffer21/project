ab4d6107f217f2928a8607e205d5f2e6c60b602b
joffer21.jrf@gmail.com <joffer21.jrf@gmail.com>
2023-05-26 12:31:57 +0100

    atualizado 2506

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Robert';
+
+  userData = {
+    email: 'exemplo@exemplo.com',
+    role: 'admin',
+  }
+
   title = 'project';
 }
